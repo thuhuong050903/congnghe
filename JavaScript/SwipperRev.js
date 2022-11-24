19e406f29811fda652538a7f59cf964b9838f95e
thuhuong050903 <116275747+thuhuong050903@users.noreply.github.com>
2022-11-24 16:59:29 +0700

    reponsive

---
+++ JavaScript/SwipperRev.js
@@ -0,0 +1,30 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    centeredSlides: true,
+    freeMode: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 1
+      },
+      700: {
+        slidesPerView: 1.5
+      }
+    }
+  });
+  
---
+++ JavaScript/reponsiveMenu.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#iconMenu').click(function() {
+        $('dropdown-menu').slideThanhmenu();
+    });
+})
